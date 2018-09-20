83db579efee85069e09b8c541a625265543edb1d
Luckyboys <eacholwong@gmail.com>
2018-09-20 17:09:39 +0800

    fix(shell): 修正了生成脚本中，生成的Windows版本，在移动后没有带后缀名，把Linux版本覆盖了的问题

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build main.go
 mv main build/redis-transmission
 CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build main.go
-mv main.exe build/redis-transmission
+mv main.exe build/redis-transmission.exe
